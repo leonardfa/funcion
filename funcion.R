51535b8efb938e71e15999cfad591e9a72d17dc2
leonardfa <74030420+leonardfa@users.noreply.github.com>
2020-11-05 20:35:54 -0500

    Add files via upload

---
+++ funcion.R
@@ -0,0 +1,45 @@
+tmb <- function(PI,TALLA,EDAD, a.fisica=c("poco/nada","ligero","moderado",
+                                          "fuerte","profesional/extremo")) {
+  #argumentos: 
+  #- PI:PESO IDEAl
+  #- TALLA: ESTATURA EN CM 
+  #- EDAD: EDAD EN AÑOS
+  #- a.fisica: ACTIVIDAD FISICA REALIZADA que puede ser uno solo de
+  #los siguientes:
+  
+  # "poco/nada"
+  # "ligero"
+  # "moderado"
+  # "fuerte"
+  # "profesional/extremo"
+  
+ #por defecto esta poco/nada
+  
+  switch (a.fisica[1], #el 1 garantiza tomar el primer elemento del vector
+           
+    "poco/nada" = {
+      r <-(65.51 +(9.56*PI) +(1.85 * TALLA) - (4.68 * EDAD))* 1.2
+    },
+    ligero= {
+      r<-(65.51 +(9.56*PI) +(1.85 * TALLA) - (4.68 * EDAD))* 1.375
+    },
+    moderado={
+      r<-(65.51 +(9.56*PI) +(1.85 * TALLA) - (4.68 * EDAD))*1.55
+    },
+    fuerte={
+      r <- (65.51 +(9.56*PI) +(1.85 * TALLA) - (4.68 * EDAD))*1.725
+    },
+    "profesional/extremo"={
+      r<-(65.51 +(9.56*PI) +(1.85 * TALLA) - (4.68 * EDAD))*1.9
+    }
+  )
+  r
+}
+
+tmb(68,170,22,"profesional/extremo")
+tmb(68,170,22,"ligero")
+tmb(68,170,22,"moderado")
+tmb(68,170,22,"fuerte")
+tmb(68,170,22,"poco/nada")
+tmb(68,170,22)
+tmb()
